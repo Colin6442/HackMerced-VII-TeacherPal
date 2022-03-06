fad973d20d1742990ad27a6120545bc3259f295e
Synthesiabladed <jay.har.dud@gmail.com>
2022-03-05 20:33:46 -0800

    Comitting a Thing

--- student.js
+++ student.js
@@ -1,4 +1,8 @@
 function JAVASCRIPTTEST(){
-    console.log("");
+    console.log("I AM SUIPPOSED TO PRINT YA KNOW");
+    console.log("BLAME COLIN IF I DON'T PRINT");
+}
 
+function addNewStudent(){
+    
 }
