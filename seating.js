72ae382301690a03eff556e25490ed63f1516697
Colin S <colin6442@gmail.com>
2022-03-06 07:49:11 -0800

    im so tired... the buttons work

--- seating.js
+++ seating.js
@@ -24,6 +24,56 @@ sliderY.oninput = function() {
   tableCreate();
 }
 
+function setStudent(){
+  fName = document.getElementById("fname").value;
+  lName = document.getElementById("lname").value;
+  currBtn = document.getElementById(isSelected());
+  currBtn.style.backgroundColor = filledSeat;
+  currBtn.innerHTML = fName;
+
+}
+
+function isSelected(){
+  for (let i = 0; i < outputX; i++) {
+    for (let j = 0; j < outputY; j++) {
+      currBtn = document.getElementById("btn" + String(outputY*i + j +1))
+      if(currBtn.style.backgroundColor == 'rgb(194, 212, 32)'){
+        return ("btn" + String(outputY*i + j +1));
+      }
+    }
+  }
+  return '1';
+}
+
+function clearSelected(){
+  for (let i = 0; i < outputX; i++) {
+    for (let j = 0; j < outputY; j++) {
+      currBtn = document.getElementById("btn" + String(outputY*i + j +1))
+      if(currBtn.style.backgroundColor == 'rgb(194, 212, 32)'){
+        currBtn.style.backgroundColor = emptySeat;
+      }
+    }
+  }
+}
+
+function selectSeat(num){
+  clearSelected();
+  currSeat = document.getElementById("currentSeat");
+  currSeat.innerHTML="Current Seat: " + String(num);
+  currGrid = document.getElementById("grid" + String(num));
+  currGrid.innerHTML='';
+  btn = document.createElement('button');
+  btn.style.width = "5vw";
+  btn.style.height = "7vh";
+  btn.style.borderRadius = "10px";
+  btn.style.backgroundColor = selectedSeat;
+  btn.setAttribute("value", String(num));
+  btn.setAttribute("id", "btn" + String(num));
+  btn.setAttribute("onClick", "selectSeat("+ String(num) +")");
+  btn.appendChild(document.createTextNode(num));
+  currGrid.appendChild(btn);
+}
+
 function tableCreate() {
   newTable = document.getElementById("seatingChart");
   tbl = document.createElement('table');
@@ -40,6 +90,7 @@ function tableCreate() {
       td.style.border = 'none';
       td.style.width = '5vw';
       td.style.height = '7vh';
+      td.setAttribute("id", "grid" + String(outputY*i + j +1));
       btn = document.createElement('button');
       btn.style.width = "5vw";
       btn.style.height = "7vh";
